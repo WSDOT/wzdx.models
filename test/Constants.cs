a1068414e91298b13ebbd588bce6737d603d7664
Russell Huebner <russell.j.huebner@outlook.com>
2022-07-09 14:07:03 -0700

    Add local schema copy to allow offline / containerized execution.

--- test/Constants.cs
+++ test/Constants.cs
@@ -13,10 +13,8 @@ namespace Wsdot.Wzdx.Models.Tests
             {
                 public static readonly Version DefaultVersion = new Version(4, 0);
                 public const string RoadEventsFeedSchema = "https://raw.githubusercontent.com/usdot-jpo-ode/wzdx/main/schemas/4.0/WZDxFeed.json";
-                //https://raw.githubusercontent.com/usdot-jpo-ode/wzdx/main/schemas/4.0/RoadEventFeature.json
                 public const string FieldDevicesFeedSchema = "https://raw.githubusercontent.com/usdot-jpo-ode/wzdx/main/schemas/4.0/SwzDeviceFeed.json";
                 public const string RoadRestrictionsEventFeedSchema = "https://raw.githubusercontent.com/usdot-jpo-ode/wzdx/main/schemas/4.0/RoadRestrictionFeed.json";
-
                 public const string FeedInfoSchema = "https://raw.githubusercontent.com/usdot-jpo-ode/wzdx/main/schemas/4.0/FeedInfo.json";
             }
         }
--- test/Core/SchemaValidator.cs
+++ test/Core/SchemaValidator.cs
@@ -2,18 +2,14 @@
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json.Schema;
 using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Net.Http;
 
 namespace Wsdot.Wzdx.Models.Tests.Core
 {
     internal sealed class SchemaValidator
     {
-        private static readonly IDictionary<Uri, SchemaValidator> KnownValidators = new ConcurrentDictionary<Uri, SchemaValidator>();
-
         private Func<string, IEnumerable<ValidationError>> Validator { get; }
 
         private SchemaValidator(JSchema schema) :
@@ -35,47 +31,25 @@ namespace Wsdot.Wzdx.Models.Tests.Core
 
         public static SchemaValidator Load(Uri path)
         {
-            SchemaValidator validator;
-            lock (KnownValidators)
-            {
-                if (KnownValidators.TryGetValue(path, out validator))
-                    return validator;
-
-                var client = new HttpClient(new CacheMessageHandler());
-                var request = client.GetAsync(path);
-                request.Wait();
-
-                if (request.IsCompletedSuccessfully)
-                {
-                    validator = Load(request.Result.Content.ReadAsStream());
-                    KnownValidators.TryAdd(path, validator);
-                }
-                else
-                {
-                    validator = Empty();
-                }
-            }
-
-            return validator;
+            return Load(path, new LocalUrlResolver());
         }
 
-        public static bool TryLoad(Uri uri, out SchemaValidator validator)
+        public static SchemaValidator Load(Uri path, JSchemaResolver resolver)
         {
-            validator = Load(uri);
-            return !validator.IsEmpty;
+            var resource = resolver.GetSchemaResource(new ResolveSchemaContext(), new SchemaReference() { BaseUri = path });
+            return Load(resource, resolver);
         }
 
-        public static SchemaValidator Load(Stream stream)
+        public static SchemaValidator Load(Stream stream, JSchemaResolver resolver)
         {
             var reader = new JsonTextReader(new StreamReader(stream));
             var schema = JSchema.Load(reader, new JSchemaReaderSettings()
             {
-                Resolver = new JSchemaUrlResolver()
+                Resolver = resolver
             });
 
             return new SchemaValidator(schema);
         }
-
         private IEnumerable<ValidationError> Validate(string json) => Validator(json);
 
         // ReSharper disable once MemberCanBePrivate.Global
@@ -89,6 +63,27 @@ namespace Wsdot.Wzdx.Models.Tests.Core
             var obj = JToken.Parse(json);
             obj.IsValid(schema, out IList<ValidationError> errors);
             return errors;
+        }
+    }
+
+    internal class LocalUrlResolver : JSchemaResolver
+    {
+        public override Stream GetSchemaResource(ResolveSchemaContext context, SchemaReference reference)
+        {
+            return TryResolveLocal(context, reference, out var content)
+                ? content
+                : throw new ArgumentException(
+                    $"No local copy of referenced url {reference.BaseUri}",
+                    nameof(reference));
+        }
+        
+        private static bool TryResolveLocal(ResolveSchemaContext context, SchemaReference reference, out Stream content)
+        {
+            var uri = reference.BaseUri ?? new Uri("http://localhost");
+            // read from the directory
+            var path = $".\\local\\{uri.Host}\\{uri.AbsolutePath}";
+            content = File.Exists(path) ? File.OpenRead(path) : new MemoryStream();
+            return content.Length > 0;
         }
     }
 }
