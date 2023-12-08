cc41146cbc61a8e1133c563faded4d5045b41b10
Russell Huebner <huebnerr@wsdot.wa.gov>
2023-12-08 11:57:26 -0800

    Removed unneed supression file for editor config

--- src/GlobalSuppressions.cs
+++
@@ -1,9 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Style", "IDE0074:Use compound assignment", Justification = "Supporting versions of C# < 8.0", Scope = "member", Target = "~M:Wzdx.v4.Devices.FieldDeviceFeatureBuilder`2.WithRoadName(System.String)~`0")]
-[assembly: SuppressMessage("Style", "IDE0074:Use compound assignment", Justification = "Supporting versions of C# < 8.0", Scope = "member", Target = "~M:Wzdx.v4.Devices.FieldDeviceFeatureBuilder`2.WithStatusMessage(System.String)~`0")]
